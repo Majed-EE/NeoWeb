9ee4559645d88d7027943e4306b9bd5068d64a74
mike ashraf <syed.majed.ashraf@gmail.com>
2025-06-27 16:53:15 +0530

    till object complete

--- my_js/test.js
+++ my_js/test.js
@@ -7,7 +7,7 @@ function nextInLine(arr, item){
 
 var testArr=[1,2,3,4,5];
 
-console.log("hi")
+
 console.log("before: "+JSON.stringify(testArr));
 // console.log(nextInLine(testArr,6));
 console.log("After: "+JSON.stringify(testArr))
@@ -38,7 +38,7 @@ function checkObj(checkProp){
 console.log(checkObj("name"))
 
 // accessing nested object
-//
+// setup 1
 var myStorage={
     "car":{
         "inside":
@@ -56,3 +56,81 @@ var myStorage={
 
 var gloveBoxContents= myStorage.car.inside["glove box"];
 console.log(`var is ${gloveBoxContents}`);
+
+//setup 2
+var myPlants=[
+    {
+        type:"flowers",
+        list:["rose","tulip", "dandelion"]
+    },
+    {
+        type:"trees",
+        list:["fir","pine","birch"]
+    }
+];
+var secondTree=myPlants[1].list[1]; // outputs pine
+
+// setup 3
+var collection = {
+    "2548": {
+        "album": "Slippery When Wet",
+        "artist": "Bon Jovi",
+        "tracks": [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    "2468": {
+        "album": "1999",
+        "artist": "Prince",
+        "tracks": [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    "1245": {
+        "artist": "Robert Palmer",
+        "tracks": []
+    },
+    "5439": {
+        "album": "ABBA Gold",
+        "artist": "ABBA",
+        "tracks": [
+            "Dancing Queen",
+            "Mamma Mia",
+            "Take a Chance on Me"
+        ]
+    },
+    "4439": {
+        "album": "Mike Gold",
+        "artist": "Mike",
+        "tracks": []
+    },
+    "5932":{
+        "album":"Sin Cara"
+    }
+};
+
+
+// keeep a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection)); // fancy way of coping an object
+
+
+
+function updateRecords(id, prop, value){
+    if (value===""){
+        delete collection[id][prop]
+    }
+    else if (prop==="tracks"){
+        collection[id][prop]=collection[id][prop] || [] ;// if collection[id][prop] exist then it is equal to itself else it is an empty list
+        collection[id][prop].push(value);
+    }else{
+        collection[id][prop]=value;
+    }
+    return collection;
+}
+
+// example test
+console.log("XXXXXXXXXXXXXX test XXXXXXXXXXXXXXXXX");
+updateRecords(2468,"tracks","Test Track");
+console.log(updateRecords(5439,"artist","ABBA"));
